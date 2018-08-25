4ea046c6e410172c379e7cecd5c4136565aad5a0
chrisobo98 <christopherbermudez98@gmail.com>
2018-08-25 03:22:26 -0600

    created recipe list to hold recipe items, created a model.ts file to create the blueprint for the recipe list, called it under recipe array in the RecipeListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  // uses my own Recipe model in recipe.model.ts
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg')
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,4 +1,18 @@
+// blueprint for object created (essentially vanilla javascript)
 export class Recipe {
+    // the recipe has to have a name
     public name: string;
+    // I also want a description on the recipe
     public description: string;
+    // since I wont store the image here, I want to store the path to the image, it will hold a url, which is a string in the end
+    public imagePath: string;
+
+    // passing the arguments right to the constructor
+    // built in function every class has, which will be executed once you create a new instace of the class
+    constructor (name: string, desc: string, imagePath: string) {
+        // I assigned the items recieved to properties of objects 
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
 }
