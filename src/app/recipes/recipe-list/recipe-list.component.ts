29ecdb5ffabf5cda8995f4524089b5c1f9d68b47
chrisobo98 <christopherbermudez98@gmail.com>
2018-08-26 07:20:06 -0600

    got a recipe to show by using string interpolation and recalling the property names set up in the recipe.model.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   // uses my own Recipe model in recipe.model.ts
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg')
+    new Recipe('Creepes', 'Crepes are french-style pancakes that are thin and flat, and can be made and filled with sweet or savory ingredients. We love them filled with nutella and strawberries or bananas, or a sweet cream filling with fruit, like in this recipe.', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg')
   ];
 
   constructor() { }
