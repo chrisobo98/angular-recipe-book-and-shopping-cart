6a510665653c98d55ffe6fb53fe5655510ef9daa
chrisobo98 <christopherbermudez98@gmail.com>
2018-08-29 13:31:37 -0600

    created button to manage recipes, made list into a bootstrap list-group with a cursor style pointer on hover

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Recipe
+} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,12 +15,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   // uses my own Recipe model in recipe.model.ts
   recipes: Recipe[] = [
-    new Recipe('Creepes', 'Crepes are french-style pancakes that are thin and flat, and can be made and filled with sweet or savory ingredients. We love them filled with nutella and strawberries or bananas, or a sweet cream filling with fruit, like in this recipe.', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg')
+    new Recipe('Creepes', 'This is a test', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg'),
+    new Recipe('Creepes', 'This is a test', 'https://www.thespruceeats.com/thmb/GNWmK7xLQfPVeKaXiNZGkCbZcE8=/2500x1668/filters:no_upscale():max_bytes(150000):strip_icc()/crepes-2500-56a210a33df78cf772719028.jpg')
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
