da28fde88fa2fa47ad13cd6ca3e7342818e38150
chrisobo98 <christopherbermudez98@gmail.com>
2018-08-30 14:14:49 -0600

    created shared folder to hold the ingredient model which will be used in the shopping list and the recipe section. Created the ingredient class and defined how it should look like.

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+// Instead of using the code below, typescript offers a shortcut
+// export class Ingredient {
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount: number) {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
+export class Ingredient {
+    constructor(public name: string, public amount: number) {}
+}
+
+// The code above does the same thing as the previous code, but it is shortened
+
