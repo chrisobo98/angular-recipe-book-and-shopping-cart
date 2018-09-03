9ccd7edfdca28a4a2ece614df79d5d8a15a56ddd
chrisobo98 <christopherbermudez98@gmail.com>
2018-09-03 00:07:05 -0600

    added working navigation, removed the search bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component ({
     selector: 'app-header',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent {
+    // Emitting the event, @Output is attached so the property can be listen to the parent component
+    @Output() featureSelected = new EventEmitter<string>();
 
-    constructor() { }
-  
-    ngOnInit() {
+    // implementing onSelect being used in header.component.html, it is a string so the feauture is string
+    onSelect(feature: string) {
+        // using the property the property that holds the event emitter as a value to emit a event whenever we click one of the buttons
+        this.featureSelected.emit(feature);
     }
-  
   }
